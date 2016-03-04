4259247d458718d59a86477426e26515219c1030
Andrew Berezin <berezinandriy@gmail.com>
2016-03-04 10:28:49 -0800

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Debugging.Startup))]
+namespace Debugging
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
